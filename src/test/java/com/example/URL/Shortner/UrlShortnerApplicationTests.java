f0b7d3cf6ed621641ad407e6cd3ca194d7afe1d1
Hassaan <sohailhassaan80@gmail.com>
2025-08-27 10:23:05 +0500

    Initial commit

---
+++ src/test/java/com/example/URL/Shortner/UrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.URL.Shortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
